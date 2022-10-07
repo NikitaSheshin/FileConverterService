19d21dcda122be48fb3e0a97f1dbd7ca43713dcc
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-10-07 16:01:59 +0300

    Добавил класс District

---
+++ src/main/java/City/District.java
@@ -0,0 +1,19 @@
+package City;
+
+import java.util.List;
+
+public class District {
+    String name;
+    List<House> houses;
+
+    public District(){
+
+    }
+
+    public District(String name, List<House>){
+        this.name = name;
+        this.houses = houses;
+    }
+
+    
+}
