520b2d03b9993b1539288e2a2b6abe0b56a9e1a5
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-10-07 15:27:40 +0300

    Добавил тесты для класса House

--- src/test/java/City/CityTest.java
+++ src/test/java/City/CityTest.java
@@ -3,6 +3,9 @@ package City;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CityTest {
     @Test
     public void CheckEntranceContructor(){
@@ -31,5 +34,42 @@ public class CityTest {
         Assert.assertEquals(2, entrance2.getCountOfCitizens());
         Assert.assertEquals(3, entrance2.getCountOfFlats());
         Assert.assertEquals(4, entrance2.getDebt());
+    }
+
+
+    @Test
+    public void CheckHouseContructor(){
+        House house1 = new House();
+        List<Entrance> entrs = new ArrayList<>();
+        entrs.add(new Entrance());
+        entrs.add(new Entrance(1, 2 ,3));
+        House house2 = new House("Советская", 54, entrs);
+
+        Assert.assertNotNull(house1);
+        Assert.assertNotNull(house2);
+    }
+
+    @Test
+    public void CheckHouseGetters(){
+        List<Entrance> entrs = new ArrayList<>();
+        entrs.add(new Entrance());
+        entrs.add(new Entrance(1, 2 ,3));
+
+
+        House house1 = new House("Советская", 54, entrs);
+
+        Assert.assertEquals(1, house1.getStreet());
+        Assert.assertEquals(1, house1.getNumber());
+        Assert.assertEquals("java.util.ArrayList", house1.getEntrances().getClass().getName());
+
+        House house2 = new House();
+
+        house2.setStreet("Советская");
+        house2.setNumber(3);
+        house2.setEntrances(entrs);
+
+        Assert.assertEquals("Советская", house2.getStreet());
+        Assert.assertEquals(3, house2.getNumber());
+        Assert.assertEquals("java.util.ArrayList", house2.getEntrances().getClass().getName());
     }
 }
