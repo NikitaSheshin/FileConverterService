607db6ac9b8bbd54ca3b368553550c395cf9b1ea
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-10-15 08:36:03 +0300

    Добавил методы для создания xml файла из считанных данных

--- src/main/java/org/example/JsonToXml.java
+++ src/main/java/org/example/JsonToXml.java
@@ -7,8 +7,22 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
+import javax.imageio.metadata.IIOMetadataNode;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,7 +30,101 @@ import java.util.List;
 
 public class JsonToXml {
     public static void convert(String fileName){
-        var t = readJsonFromFile(fileName);
+        var districts = readJsonFromFile(fileName);
+
+        DocumentBuilderFactory dbf;
+        DocumentBuilder db ;
+        Document doc;
+
+        dbf = DocumentBuilderFactory.newInstance();
+        try {
+            db  = dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+        doc = db.newDocument();
+
+        Element root = doc.createElement("database");
+
+        for (var district : districts)
+            root.appendChild(createDistrict(district, doc));
+
+        doc.appendChild(root);
+
+        Transformer trf = null;
+        DOMSource src = null;
+        FileOutputStream fos = null;
+
+        try {
+            trf = TransformerFactory.newInstance().newTransformer();
+        } catch (TransformerConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+        src = new DOMSource(doc);
+        try {
+            fos = new FileOutputStream("xmlRes.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        StreamResult result = new StreamResult(fos);
+        try {
+            trf.transform(src, result);
+        } catch (TransformerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Node createDistrict(District district, Document doc){
+
+        Element districtNode = doc.createElement("district");
+        Element name = doc.createElement("name");
+        name.setTextContent(district.getName());
+        districtNode.appendChild(name);
+
+        for(int i = 0; i < district.getHouses().size(); ++i)
+            districtNode.appendChild(createHouse(district.getHouses().get(i), i, doc));
+
+        return districtNode;
+    }
+
+    private static Node createHouse(House house, int id, Document doc){
+        Element houseNode = doc.createElement("house");
+        houseNode.setAttribute("id", String.valueOf(id));
+
+        Element street = doc.createElement("street");
+        street.setTextContent(house.getStreet());
+
+        Element number = doc.createElement("number");
+        number.setTextContent(String.valueOf(house.getNumber()));
+
+        houseNode.appendChild(street);
+        houseNode.appendChild(number);
+
+        for(int i = 0; i < house.getEntrances().size(); ++i)
+            houseNode.appendChild(createEntrance(house.getEntrances().get(i), i, doc));
+
+        return houseNode;
+    }
+
+    private static Node createEntrance(Entrance entrance, int id, Document doc){
+        Element entranceNode = doc.createElement("entrance");
+        entranceNode.setAttribute("id", String.valueOf(id));
+
+        Element countOfFlats = doc.createElement("countOfFlats");
+        countOfFlats.setTextContent(String.valueOf(entrance.getCountOfFlats()));
+
+        Element countOfCitizens = doc.createElement("countOfCitizens");
+        countOfCitizens.setTextContent(String.valueOf(entrance.getCountOfCitizens()));
+
+        Element debt = doc.createElement("debt");
+        debt.setTextContent(String.valueOf(entrance.getDebt()));
+
+        entranceNode.appendChild(countOfFlats);
+        entranceNode.appendChild(countOfCitizens);
+        entranceNode.appendChild(debt);
+
+        return entranceNode;
     }
 
     private static List<District> readJsonFromFile(String fileName){
