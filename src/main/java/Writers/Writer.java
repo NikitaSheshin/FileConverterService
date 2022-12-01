7fa89c9ccc0c7e7c267dfc264416148552fe534c
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-12-01 23:12:21 +0300

    Добавил чтение из xml при помощи jaxb

--- src/main/java/Writers/Writer.java
+++ src/main/java/Writers/Writer.java
@@ -2,8 +2,9 @@ package Writers;
 
 import City.District;
 
+import javax.xml.bind.JAXBException;
 import java.util.List;
 
 public interface Writer {
-    void writeToFile(String fileName, List<District> districts);
+    void writeToFile(String fileName, List<District> districts) throws JAXBException;
 }
--- src/main/java/Writers/WriterToXmlWithJaxb.java
+++ src/main/java/Writers/WriterToXmlWithJaxb.java
@@ -1,2 +1,23 @@
-package Writers;public class WriterToXmlWithJaxb {
+package Writers;
+
+import City.District;
+import City.DistrictsStore;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+import java.util.List;
+
+public class WriterToXmlWithJaxb implements Writer {
+    @Override
+    public void writeToFile(String fileName, List<District> districts) throws JAXBException {
+        DistrictsStore store = new DistrictsStore();
+        store.setDistricts(districts);
+
+        JAXBContext context = JAXBContext.newInstance(DistrictsStore.class);
+        Marshaller mar = context.createMarshaller();
+        mar.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        mar.marshal(store, new File(fileName));
+    }
 }
