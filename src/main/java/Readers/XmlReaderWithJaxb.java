326f105658c95c1ddbdb8d6b93890bc223689ebd
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-12-01 13:02:42 +0300

    Добавил класс XmlReaderWithJaxb для чтения xml-файлов с помощью jaxb

---
+++ src/main/java/Readers/XmlReaderWithJaxb.java
@@ -0,0 +1,24 @@
+package Readers;
+
+import City.District;
+import City.DistrictsStore;
+import City.Entrance;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class XmlReaderWithJaxb implements Reader {
+    public List<District> readFromFile(String fileName) throws JAXBException, FileNotFoundException {
+        var context = JAXBContext.newInstance(DistrictsStore.class);
+        var um = context.createUnmarshaller();
+
+        return ((DistrictsStore) um.unmarshal(new InputStreamReader(
+                new FileInputStream(fileName), StandardCharsets.UTF_8))).getDistricts();
+    }
+}
