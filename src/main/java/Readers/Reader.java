dce60db5b7db52d65b5cedc9def09853be1ec06b
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-12-01 23:07:57 +0300

    Убрал лишний импорт

--- src/main/java/Readers/Reader.java
+++ src/main/java/Readers/Reader.java
@@ -2,8 +2,11 @@ package Readers;
 
 import City.District;
 
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public interface Reader {
-    List<District> readFromFile(String fileName);
+    List<District> readFromFile(String fileName) throws JAXBException, FileNotFoundException;
 }
+
