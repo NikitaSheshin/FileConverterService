abdcb13938235a199d5efd060e5aa7246eaa9a46
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-12-01 19:08:25 +0300

    Убрал лишние import из класса

--- src/main/java/Readers/XmlReaderWithJaxb.java
+++ src/main/java/Readers/XmlReaderWithJaxb.java
@@ -2,7 +2,6 @@ package Readers;
 
 import City.District;
 import City.DistrictsStore;
-import City.Entrance;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -10,7 +9,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 
 public class XmlReaderWithJaxb implements Reader {
