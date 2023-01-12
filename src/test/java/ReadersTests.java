5fd5b7d5d1f12a1c521da6f6600e208798ff19a3
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2023-01-12 21:49:19 +0300

    Убраны лишние тесты

--- src/test/java/ReadersTests.java
+++ src/test/java/ReadersTests.java
@@ -14,12 +14,9 @@ public class ReadersTests {
     private Reader reader;
 
     private final static String PATH_TO_CORRECT_XML_FILE = "src\\test\\resources\\files\\CorrectXml.xml";
-    private final static String PATH_TO_INCORRECT_XML_FILE = "src\\test\\resources\\\\WrongXml.xml";
 
     private final static String PATH_TO_CORRECT_JSON_FILE = "src\\test\\resources\\files\\CorrectJson.json";
-    private final static String PATH_TO_INCORRECT_JSON_FILE = "src\\test\\resources\\files\\WrongJson.json";
 
-    private final static String WRONG_PATH = "wrong path";
     private final static int COUNT_OF_DISTRICTS_IN_CORRECT_FILE = 5;
 
     @Test
@@ -30,35 +27,11 @@ public class ReadersTests {
         assertEquals(dataFromFile.size(), COUNT_OF_DISTRICTS_IN_CORRECT_FILE);
     }
 
-    @Test(expected = IllegalCallerException.class)
-    public void jaxbReaderWithWrongPath() throws JAXBException, IOException {
-        reader = new XmlReader();
-        reader.readFromFile(WRONG_PATH);
-    }
-
-    @Test(expected = IllegalCallerException.class)
-    public void jaxbReaderWithBadStructure() throws JAXBException, IOException {
-        reader = new XmlReader();
-        reader.readFromFile(PATH_TO_INCORRECT_XML_FILE);
-    }
-
     @Test
     public void jsonReaderTest() throws JAXBException, IOException {
         reader = new JsonReader();
         var dataFromFile = reader.readFromFile(PATH_TO_CORRECT_JSON_FILE);
 
         assertEquals(dataFromFile.size(), COUNT_OF_DISTRICTS_IN_CORRECT_FILE);
-    }
-
-    @Test(expected = IllegalCallerException.class)
-    public void jsonReaderTestWithWrongPath() throws JAXBException, IOException {
-        reader = new JsonReader();
-        reader.readFromFile(WRONG_PATH);
-    }
-
-    @Test(expected = JsonSyntaxException.class)
-    public void jsonReaderTestWithWrongStructure() throws JAXBException, IOException {
-        reader = new JsonReader();
-        reader.readFromFile(PATH_TO_INCORRECT_JSON_FILE);
     }
 }
