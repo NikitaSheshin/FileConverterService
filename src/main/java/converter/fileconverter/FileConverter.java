c1b938632c561b69b66abf0d14e5c91ff19c22d9
NikitaSheshin <sheshin.nikita21@yandex.ru>
2023-01-31 11:34:58 +0300

    При попытке передачи в конвертер некорректных аргументов программа больше не заканчиавет работу

--- src/main/java/converter/fileconverter/FileConverter.java
+++ src/main/java/converter/fileconverter/FileConverter.java
@@ -6,12 +6,31 @@ import javax.xml.bind.JAXBException;
 import java.io.IOException;
 
 public class FileConverter {
+    private static int countOfFiles = 0;
+    private static int countOfErrors = 0;
+
+    public static void setStart() {
+        countOfFiles = 0;
+        countOfErrors = 0;
+    }
+
     static public void convert(final String[] fileNames) throws JAXBException, IOException {
         val arguments = new InputArguments(fileNames);
 
-        WriterCreator.create(arguments.getOutputFileName())
+        if (arguments.isValid()) {
+            WriterCreator.create(arguments.getOutputFileName())
                 .writeToFile(arguments.getOutputFileName(),
-                        ReaderCreator.create(arguments.getInputFileName())
-                                .readFromFile(arguments.getInputFileName()));
+                    ReaderCreator.create(arguments.getInputFileName())
+                        .readFromFile(arguments.getInputFileName()));
+        } else {
+            countOfErrors++;
+        }
+
+        countOfFiles++;
+    }
+
+    public static void getStatistic() {
+        System.out.println("Обработано " + countOfFiles + " файлов\n" +
+            "Из них с ошибкой - " + (countOfFiles - countOfErrors));
     }
 }
--- src/main/java/converter/fileconverter/InputArguments.java
+++ src/main/java/converter/fileconverter/InputArguments.java
@@ -15,36 +15,50 @@ public class InputArguments {
     private final static String WRONG_FORMAT_MESSAGE = "Переденные файлы должны иметь разрешения .xml и .json";
 
     public InputArguments(@NonNull String[] args) {
-        checkCorrectCount(args);
-        inputFileName = args[0];
-        outputFileName = args[1];
-        checkNotNullArguments();
-
-        checkFilesFormats();
+        if (checkCorrectCount(args)) {
+            inputFileName = args[0];
+            outputFileName = args[1];
+        } else {
+            inputFileName = null;
+            outputFileName = null;
+        }
     }
 
-    private void checkCorrectCount(final String[] args) {
+    public boolean isValid() {
+        return checkFilesFormats() && checkNotNullArguments();
+    }
+
+    private boolean checkCorrectCount(final String[] args) {
         if (args.length < 2) {
             log.warn(WRONG_COUNT_OF_ARGUMENTS_MESSAGE);
             System.out.println(WRONG_COUNT_OF_ARGUMENTS_MESSAGE);
-            throw new IllegalArgumentException(WRONG_COUNT_OF_ARGUMENTS_MESSAGE);
+
+            return false;
         }
+
+        return true;
     }
 
-    private void checkNotNullArguments() {
+    private boolean checkNotNullArguments() {
         if (inputFileName == null || outputFileName == null) {
             log.warn(NULL_ARGUMENTS_MESSAGE);
             System.out.println(NULL_ARGUMENTS_MESSAGE);
-            throw new IllegalArgumentException(NULL_ARGUMENTS_MESSAGE);
+
+            return false;
         }
+
+        return true;
     }
 
-    private void checkFilesFormats() {
+    private boolean checkFilesFormats() {
         if (!isXmlToJson() && !isJsonToXml()) {
             log.warn(WRONG_FORMAT_MESSAGE);
             System.out.println(WRONG_FORMAT_MESSAGE);
-            throw new IllegalArgumentException(WRONG_FORMAT_MESSAGE);
+
+            return false;
         }
+
+        return true;
     }
 
     private Boolean isXmlToJson() {
